26b707e3769467a0beb54d1d65f30c02a3d28753
Van Salido <vansalido@gmail.com>
2023-03-03 06:45:39 +0800

    update

--- db.py
+++ db.py
@@ -1,5 +1,5 @@
 import pymongo
-from typing import Dict, Any, List, Optional
+from typing import Dict, Any, List
 import os
 import json
 from bson import ObjectId
@@ -16,40 +16,36 @@ class MainDB:
             self.client: pymongo.MongoClient = pymongo.MongoClient(os.environ['DB_URL'])
         self.db: pymongo.database.Database = self.client.get_database()
         
+        
     def to_collection(self, collection:str)-> Any:
         self.collection: pymongo.collection.Collection = self.db.get_collection(collection)
         return self.collection
 
+
     def create(self, document: Dict[str, Any]) -> Any:
-        if document.get('password'):
-            document['password'] = self.encode_password(document.get('password'))
         result: pymongo.results.InsertOneResult = self.collection.insert_one(document)
-        return result.inserted_id
+        return self.read({'_id':result.inserted_id})
+
 
     def read(self, query: Dict[str, Any],**kwargs) -> Dict[str, Any]:
-        """add `exclude=[password,...yourfield]` to exclude fields, default `['password']`"""
-        exclude = kwargs.get('exclude',['password'])
+        """add `exclude=[password,...yourfield]` to exclude fields"""
+        exclude = kwargs.get('exclude',['_'])
 
         exempted = self.exempt_fields(exclude)
         result: pymongo.cursor.Cursor = self.collection.find_one(query,exempted)
         return dict(result)
     
+
     def read_all(self,**kwargs) -> List[Dict[str, Any]]:
-        """add `exclude=[password,...yourfield]` to exclude fields, default `['password']`"""
-        exclude = kwargs.get('exclude',['password'])
+        """add `exclude=[password,...yourfield]` to exclude fields"""
+        exclude = kwargs.get('exclude',['_'])
         
         exempted = self.exempt_fields(exclude)
         result: pymongo.cursor.Cursor = self.collection.find({},exempted)
         return list(result)
     
-    def update(self, query: Dict[str, Any], update: Dict[str, Any]) -> bool:
-        if update.get('password'):
-            if self.authenticate_password(query,update.get('old_password')):
-                update['password'] = self.encode_password(update.get('password'))
-                del update['old_password']
-            else:
-                return False
-            
+
+    def update(self, query: Dict[str, Any],update: Dict[str, Any]) -> bool:
         # Ensure that update uses valid MongoDB update syntax
         if not any(key.startswith('$') for key in update.keys()):
             update = {'$set': update}
@@ -57,21 +53,25 @@ class MainDB:
         self.collection.update_many(query, update)
         return True
 
+
     def delete(self, query: Dict[str, Any]) -> int:
         result: pymongo.results.DeleteResult = self.collection.delete_many(query)
         return result.deleted_count
     
+
     def encode_password(self,string:str)->bytes:
         password_encoder = Fernet(os.environ['FERNET_KEY'])
         return password_encoder.encrypt(string.encode('utf-8'))
 
-    def decode_password(self,bytes:bytes)->str:
+
+    def decode_password(self,bytevalue:bytes)->str:
         password_decoder = Fernet(os.environ['FERNET_KEY'])
-        return password_decoder.decrypt(bytes).decode('utf-8')
+        return password_decoder.decrypt(bytevalue).decode('utf-8')
+
 
     def authenticate_password(self,query:dict,entry:str) -> bool:
-        old_pass:bytes = self.read(query,exclude=['_'])['password']
-        return self.decode_password(old_pass) == entry
+        current_password:bytes = self.read(query,exclude=['_'])['password']
+        return self.decode_password(current_password) == entry
         
 
     def exempt_fields(self,exemptions:List[str])->dict:
@@ -79,9 +79,12 @@ class MainDB:
         for field in exemptions:
             projection[field]=False
         return projection
+    
+    def byte_to_string(self,value:bytes)->str:
+        return value.decode('utf-8')
 
     class OBjectIDtoStringConverter(json.JSONEncoder):
-        def default(self, obj):
+        def default(self, obj)->str:
             if isinstance(obj, ObjectId):
                 return str(obj)
             return json.JSONEncoder.default(self, obj)
--- routers/users/helpers.py
+++ routers/users/helpers.py
@@ -1,7 +1,48 @@
 from db import MainDB
-from typing import Dict,Any
+from typing import Dict,Any,List
 import json
+from .schema_users import UserCreationInput
 
+
+def get_all_users()->List[dict|Any]:
+    db = connect_to_database_collection_users()
+    users = db.read_all()
+    return users
+
+def get_user(username:str)->dict|None:
+    db = connect_to_database_collection_users()
+    return check_existing_username(username,db)
+
+def create_user(userdata:UserCreationInput)->dict:
+    db = connect_to_database_collection_users()
+    if check_existing_username(userdata.username,db):
+        raise ValueError(f"{userdata.username} already existed in database")
+    secured_userdata = handle_with_secure_user_creation(userdata.jsonify(),db)
+    return db.create(secured_userdata)
+
+def update_user(username:str,update:dict)->dict:
+    query = {'username':username}
+    db = connect_to_database_collection_users()
+    secured_update = handle_with_secure_user_update(query,update,db)
+    return db.update(query,secured_update)
+
+def handle_with_secure_user_creation(document:dict,db:MainDB)->dict:
+    if document.get('password'):
+        document['password'] = db.encode_password(document.get('password'))
+    return document
+
+def handle_with_secure_user_update(query:dict,update:dict,db:MainDB)->dict:
+    if update.get('new_password') and update.get('password'):
+        if db.authenticate_password(query,update.get('password')):
+            update['password'] = db.encode_password(update.get('new_password'))
+            del update['new_password']
+        else:
+            raise ValueError('Wrong password!')
+    elif update.get('password') and not update.get('new_password'):
+        del update['password']
+    elif not update.get('password') and update.get('new_password'):
+        del update['new_password']
+    return update
 
 def get_json_string_of_user(username:str)->str:
     db = connect_to_database_collection_users()
@@ -9,12 +50,8 @@ def get_json_string_of_user(username:str)->str:
     return json.dumps(userdetails,cls=db.OBjectIDtoStringConverter)
 
 def create_user_and_get_json_string(userdata:dict)->str:
-    username = userdata['username']
-    db = connect_to_database_collection_users()
-    if check_existing_username(username,db):
-        return None
-    db.create(userdata)
-    return get_json_string_of_user(username)
+    create_user(userdata)
+    return get_json_string_of_user(userdata['username'])
 
 def connect_to_database_collection_users()->MainDB:
     db:MainDB = MainDB()
