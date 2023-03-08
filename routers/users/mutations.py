2ce07d7a2def39724c74e5d204b3d92dc1dcdb1a
Van Salido <vansalido@gmail.com>
2023-03-09 05:31:20 +0800

    added token requirements

--- routers/users/mutations.py
+++ routers/users/mutations.py
@@ -3,16 +3,24 @@ import strawberry
 from . import helpers
 from .types import User,UserCreationInput,UserLog, AccessToken
 import datetime
-import jwt_auth
+from jwt_auth import require_token, encode_jwt_token
 
 @strawberry.type
 class Mutation:
+
+    @strawberry.mutation
+    def get_access_token(self,info,username:str,password:str)-> AccessToken:
+        if helpers.validate_user(username,password):
+            return AccessToken(username=username,access_token=encode_jwt_token(username))
+        raise ValueError('Wrong password')
+    
     @strawberry.mutation
     def create_user(self, info, userdata:UserCreationInput) -> User:
         created = helpers.add_user_to_database(userdata)   
         return User(**created)
     
-    @strawberry.mutation 
+    @strawberry.mutation
+    @require_token
     def update_user(self,info,username:str,update:str)-> User:
         """When updating password, include both fields: `password` and `new_password`"""
         jsoned_update = json.loads(update)
@@ -26,17 +34,13 @@ class Mutation:
         return User(**updated_user)
     
     @strawberry.mutation
-    def get_access_token(self,info,username:str,password:str)-> AccessToken:
-        if helpers.validate_user(username,password):
-            return AccessToken(username=username,access_token=jwt_auth.encode_jwt_token(username))
-        raise ValueError('Wrong password')
-
-    @strawberry.mutation
+    @require_token
     def login_user(self, info, username:str)-> User:
         logged_user = helpers.handle_login(username)  
         return User(**logged_user)
 
     @strawberry.mutation
+    @require_token
     def logout_user(self, info, username:str)-> UserLog:
         summary = helpers.handle_logout(username)
         return UserLog(**summary)
--- routers/users/queries.py
+++ routers/users/queries.py
@@ -15,9 +15,11 @@ class Query:
         return [User(**user) for user in helpers.get_all_users_from_database()]
 
     @strawberry.field
+    @require_token
     def user(self, info, username: str)-> User:
         return User(**helpers.get_user_from_database(username))
     
     @strawberry.field
+    @require_token
     def all_user_logs(self, info, username:str)-> List[UserLog]:
         return [UserLog(**userlogs) for userlogs in loggings.get_user_logs_all(username)]
